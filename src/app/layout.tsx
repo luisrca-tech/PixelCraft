747d0a0dc03e74695ed554507c272c26e57af0a9
Luis Felipe <luisrochacruzalves@gmail.com>
2024-11-01 14:24:48 -0300

    🐛 Fix: resolve wrong import from og image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,8 @@ import { Toaster } from "sonner";
 import { TRPCReactProvider } from "~/trpc/react";
 import { ClerkProvider } from "@clerk/nextjs";
 import { globalStyle } from "~/styles/global";
-import ogImage from "~/assets/images/og.png";
+import ogImage from "./public/og-image.png"
+
 export const metadata: Metadata = {
   title: "PixelCraft",
   description:
