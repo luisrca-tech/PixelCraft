9703a798b9c36f350b9609cfd778fa80b64060b2
P4vanzinho <thiago772000@gmail.com>
2025-02-17 09:45:14 -0300

    fix : fix the spacing

--- src/components/forms/FormForPeople/styles.ts
+++ src/components/forms/FormForPeople/styles.ts
@@ -6,7 +6,7 @@ export const Container = styled.div`
   flex-direction: column;
   gap: 1rem;
   width: 100%;
-  height: calc(100vh - 37rem);
+  height: calc(100vh - 33rem);
 `;
 
 export const RoleAndPerson = styled.div`
@@ -20,11 +20,20 @@ export const RoleAndPerson = styled.div`
     color: ${theme.COLORS.DARK};
     font-weight: 600;
     line-height: 28px;
+    width: 100%;
 
     @media(min-width: 1024px){
       font-size: 1.2rem;
     }
   }
+
+  span:nth-child(2),
+  span:nth-child(3) {
+    text-align: center;
+    display: flex; 
+    justify-content: center;
+  }
+  
 `;
 
 export const Form = styled.form`
@@ -51,7 +60,7 @@ export const PersonByRole = styled.div`
   justify-content: space-between;
   background-color: ${theme.COLORS.LIGHT};
   border-radius: 60px;
-
+  margin-bottom: 1rem;
   padding: 0.4rem 0.75rem;
 
   input,
@@ -60,6 +69,10 @@ export const PersonByRole = styled.div`
     color: ${theme.COLORS.SECONDARY_DARK};
     line-height: 24px;
     font-weight: 500;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 
     @media(min-width: 1024px){
       font-size: 1.2rem;
@@ -67,7 +80,7 @@ export const PersonByRole = styled.div`
   }
 
   input {
-    width: 50%;
+  
     border: none;
     background-color: transparent;
     outline: none;
