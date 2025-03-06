2667ce313c995a138690cdfcc1f7029e690b5d9e
P4vanzinho <thiago772000@gmail.com>
2025-03-06 12:36:39 -0300

    delete : remove duplicated functions

--- src/app/utils/functions/getLastRowIndex.tsx
+++
@@ -1,8 +0,0 @@
-import { rowsAndSelectedValuesAtom } from "~/@atom/ProjectStates/rowsAndSelectedValuesAtom";
-import { useAtom } from "jotai";
-
-export function useGetLastRowIndex() {
-  const [rowsAndSelectedValues] = useAtom(rowsAndSelectedValuesAtom);
-  const rows = rowsAndSelectedValues.rows;
-  return rows[rows.length - 1];
-}
--- src/app/utils/functions/isSelectOpen.tsx
+++
@@ -1,7 +0,0 @@
-import { useAtom } from "jotai";
-import { selectedItemIndexAtom } from "~/@atom/ProjectStates/selectedItemIndexAtom";
-
-export function useIsSelectOpen(index: string) {
-  const [selectedItemIndex] = useAtom(selectedItemIndexAtom);
-  return selectedItemIndex === index;
-}
