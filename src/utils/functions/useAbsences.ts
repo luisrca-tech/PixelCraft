69951eb04543ebf2a24b6b5f42681946d04357ad
P4vanzinho <thiago772000@gmail.com>
2025-03-05 21:27:23 -0300

    feat : create hook for absences form

---
+++ src/utils/functions/useAbsences.ts
@@ -0,0 +1,58 @@
+import { useMemo } from "react";
+import { api } from "~/trpc/react";
+import { type TaskInfo } from "~/server/types/Clickup.type";
+import { type AbsencesData } from "~/types/absenses-input-type";
+import { useGetInputValueAtIndex } from "~/utils/functions/getInputValueAtIndex";
+
+export const useAbsences = (task: TaskInfo, reset: (data: AbsencesData) => void) => {
+    const projectHeaderInputValue = useGetInputValueAtIndex(
+        undefined,
+        "projectRow",
+        true
+    );
+
+    const {
+        data: absences,
+        isLoading: absencesIsLoading,
+        isFetched: absencesIsFetched,
+    } = api.absences.getAbsencesByProjectName.useQuery({
+        projectName: projectHeaderInputValue || "",
+    });
+
+    const formattedAbsences = useMemo(() => {
+        if (absencesIsFetched && absences) {
+            const formattedData = {
+                absencesForTask: task.months.map((month) => {
+                    const year = month.split("-")[1] || "0";
+                    const yearNumber = parseInt(year);
+
+                    const absenceForMonth = absences.find(
+                        (absence) =>
+                            absence.taskId === task.taskId &&
+                            absence.month.toString().padStart(2, "0") ===
+                            month.split("-")[0] &&
+                            absence.year === yearNumber
+                    );
+
+                    return {
+                        taskId: task.taskId,
+                        month: month,
+                        absences: absenceForMonth?.absences?.toString() || "0",
+                    };
+                }),
+            };
+
+            // Executa o reset com os dados formatados
+            reset(formattedData);
+
+            return formattedData;
+        }
+        return null;
+    }, [absencesIsFetched, absences, task.months, task.taskId, reset]);
+
+    return {
+        absences: formattedAbsences,
+        isLoading: absencesIsLoading,
+        isFetched: absencesIsFetched,
+    };
+};
